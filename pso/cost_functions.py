e1aa197208a6b9f4fde04d27b9b3233d0d340216
Will Griffin <will@grifffin.uk>
2024-02-19 19:02:45 +0000

    Implementation of Dixon-Price and Zakharov functions into Nathans Rooys Partical Swarm Optimization

--- pso/cost_functions.py
+++ pso/cost_functions.py
@@ -1,8 +1,30 @@
-def sphere(x):
-    total=0
-    for i in range(len(x)):
-        total+=x[i]**2
-    return total
-    
-if __name__ == "pso.sphere":
-    sphere()
+
+
+N = 20  
+
+def dixonPriceFunc(x):
+    utility = (x[0] - 1)**2
+    for i in range(1, N):
+        utility += i * (2 * x[i]**2 - x[i-1])**2
+    return utility
+
+
+def zakharovFunc(x):
+
+    # Calculate sigma1
+    sigma1 = 0
+    for i in range(N):
+        sigma1 += x[i]**2
+
+    # Calculate sigma2
+    sigma2 = 0
+    for i in range(N):
+        sigma2 += 0.5 * (i + 1) * x[i]
+
+    # Calculate utility
+    utility = sigma1 + sigma2**2 + sigma2**4
+    return utility
+
+if __name__ == "__main__":
+    dixonPriceFunc()
+    zakharovFunc()
--- tests/test_pso.py
+++ tests/test_pso.py
@@ -1,12 +1,22 @@
 import unittest
 from pso import pso_simple
-from pso.cost_functions import sphere
+from pso.cost_functions import dixonPriceFunc, zakharovFunc  
+from random import uniform
 
 
 class test_all(unittest.TestCase):
 
-    def test_everything(self):
-        x0=[5,5]
-        bounds=[(-10,10),(-10,10)]
-        opt = pso_simple.minimize(sphere, x0, bounds, num_particles=15, maxiter=30, verbose=False)
-        self.assertLess(sphere(opt[1]), sphere(x0))
+    def test_dixonPriceFunc(self):
+        x0 = [uniform(-10, 10) for x in range(20)]
+        bounds = [(-10, 10)] * 20  
+        opt = pso_simple.minimize(dixonPriceFunc, x0, bounds, num_particles=171, maxiter=400, verbose=True)
+        self.assertLess(dixonPriceFunc(opt[1]), dixonPriceFunc(x0))
+
+    def test_zakharovFunc(self):
+        x0 = [uniform(-5, 10) for x in range(20)]
+        bounds = [(-5, 10)] * 20  
+        opt = pso_simple.minimize(zakharovFunc, x0, bounds, num_particles=178, maxiter=400, verbose=True)
+        self.assertLess(zakharovFunc(opt[1]), zakharovFunc(x0))
+
+if __name__ == '__main__':
+    unittest.main()
